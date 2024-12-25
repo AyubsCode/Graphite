1b6c78d9c6083e362c29ad07dba1fc47e663109f
AyubsWrld <ayubfmohammed@gmail.com>
2024-12-25 02:07:39 -0700

    Created a timer function to learn Configuration Structures

--- esp-idf/Graphite/main/Graphite.c
+++ esp-idf/Graphite/main/Graphite.c
@@ -1,10 +1,30 @@
-#include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-
 #include "esp_log.h"
+#include "esp_timer.h"
 
-void app_main(void){
-    char* myTask = pcTaskGetName(NULL) ;
-    ESP_LOGI(myTask , "Hello world") ;
+#define TAG "Timer"
+
+void callback(void* arg){
+    static int counter ;
+    if(counter <= 30){
+        ESP_LOGI(TAG , "Logged : %d" , ++counter) ;
+    }
+    else{
+        ESP_LOGI(TAG , "Timer Concluded") ;
+        esp_timer_stop(*(esp_timer_handle_t*)arg) ;
+    }
+}
+
+void app_main(){
+
+    esp_timer_handle_t  timer ;
+    esp_timer_create_args_t timer_args = {
+        .callback = &callback,
+        .arg = &timer,
+        .name = "Timer"
+    };
+
+    esp_timer_create(&timer_args , &timer) ;
+    esp_timer_start_periodic(timer , 1000000) ;
 }
