31e9a1f392ca4db1b637baebdcd06c8e6177dbbe
AyubsWrld <ayubfmohammed@gmail.com>
2025-01-23 10:38:33 -0700

    Header file inclusion , increased modularity of the sdcard sourefile

--- esp-idf/sdcard/main/io.c
+++ esp-idf/sdcard/main/io.c
@@ -1,3 +1,19 @@
+
+#include "stdio.h"
+#include "string.h"
+#include "driver/gpio.h"
+#include "esp_log.h"
+#include "esp_system.h"
+#include "sdmmc_cmd.h"
+#include "esp_vfs.h"
+#include "esp_vfs_fat.h"
+#include "driver/sdmmc_host.h"
+#include "driver/sdspi_host.h"
+#include "driver/spi_common.h"
+#include "sdmmc_cmd.h"
+#include "esp_err.h"
+#include <stdio.h>
+#include "io.h"
 // Define pins for SPI
 #define PIN_NUM_MISO  19
 #define PIN_NUM_MOSI  23
